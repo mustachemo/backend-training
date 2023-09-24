484f4c3cb954916b4e5c515e19c6af335b77e796
mustachemo <mhasan1080@gmail.com>
2023-09-23 17:26:46 -0700

    changed stack

--- src/app.js
+++ src/app.js
@@ -2,25 +2,27 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import dotenv from 'dotenv';
-import ejs from 'ejs';
+import nunjucks from 'nunjucks';
 import logger from 'morgan';
-import indexRoute from './routes/index.js';
 
 dotenv.config();
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
-console.log('filename', __filename);
 const __dirname = path.dirname(__filename);
-console.log('dirname', __dirname);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+nunjucks.configure(path.join(__dirname, 'views'), {
+  autoescape: true,
+  express: app,
+});
+app.set('view engine', 'njk');
 app.set('port', process.env.PORT);
 
 app.use('/public', express.static('public'));
 app.use(logger('dev'));
 
-app.use('/', indexRoute);
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 export default app;
--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const indexRoute = express.Router();
-
-indexRoute.get('/', (req, res) => {
-  res.render('index');
-});
-
-export default indexRoute;
